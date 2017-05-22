4b8e4e4467aacdc11d88d422483a134e11d5e35f
Yogananda <chetan.yogananda@honeywell.com>
2017-05-22 16:36:14 +0530

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { MdCoreModule,MdSidenavModule,MdMenuModule } from '@angular/material';
+import {SlimScroll} from 'angular2-slimscroll';
+
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './home/header/header.component';
+import { LeftsidebarComponent } from './home/leftsidebar/leftsidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    HeaderComponent,
+    LeftsidebarComponent
+    
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    NgbModule.forRoot(),
+    MdCoreModule,
+    MdSidenavModule,
+    MdMenuModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  
 })
+
 export class AppModule { }
---
+++ src/app/home/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, NgModule } from '@angular/core';
+import { MdToolbarModule } from '@angular/material';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+
+
+
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+
